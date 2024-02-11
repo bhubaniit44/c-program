f319c6ee8575a5db2d5f9a3755c59a04fb3fb8a3
bhubaniit44 <u22cse1044@cit.ac.in>
2024-02-11 23:57:15 +0530

    print integer value in c

---
+++ basic of c program/user.c
@@ -0,0 +1,13 @@
+//c program to  print
+//integer value
+#include<stdio.h>
+//Drivar code
+int main()
+{
+    //declring integer 
+    int x=5;
+    //printing value
+    printf("Printing integer %d",x);
+    
+    return 0;
+}
