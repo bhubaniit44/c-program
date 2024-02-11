bcb871a1d3cc46687d0b415bbd000e07ca8e691c
bhubaniit44 <u22cse1044@cit.ac.in>
2024-02-12 01:03:53 +0530

    add two number

---
+++ basic of c program/addtwonumber.c
@@ -0,0 +1,18 @@
+//Add Two number
+#include<stdio.h>
+int main()
+{
+
+    int A , B, sum=0;
+    //Ask The user enter two number 
+    printf("Enter the number A and B:");
+    //Red Two number from The user 
+    scanf("%d%d",&A,&B);
+    //calcuting the addition A and B
+    //using "+" operator
+    sum=A+B;
+    //print the sum
+    printf("sum of A and B is:%d",sum);
+    return 0;
+
+}
