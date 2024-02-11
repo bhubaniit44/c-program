a07a6f38a583ba784682651c04578cd5d02c3cdc
bhubaniit44 <u22cse1044@cit.ac.in>
2024-02-11 23:28:25 +0530

    print name mobil number DOB

---
+++ basic of c program/print.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    //print name 
+    printf("name:Bhuban chetry\n");
+    //print mobil nuber 
+    printf("mobile number:6000548660\n");
+    //print date of birth
+    printf("DOB:24/10/2003\n ");
+    return 0;
+}
